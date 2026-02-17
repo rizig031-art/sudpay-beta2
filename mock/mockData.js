770fbb887dd742fcc36acd21c6430df102e0bbc4
rizig031-art <rizig031@gmail.com>
2026-02-18 01:42:25 +0200

    Create mockData.js

---
+++ mock/mockData.js
@@ -0,0 +1,8 @@
+export const banks = ["BancYouk", "OCash", "Fawry", "Sahl", "RetaPay", "MyCash"];
+
+export const initialTransactions = [
+  { id: 1, type: "Payment", amount: 5000, bank: "BancYouk", date: "2026-02-18 10:00" },
+  { id: 2, type: "Recharge", amount: 1000, bank: "OCash", date: "2026-02-18 11:00" },
+];
+
+export const initialBalance = 25000;
